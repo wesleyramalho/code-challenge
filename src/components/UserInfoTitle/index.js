58ea997b674907a518a315189c2116f8d2a96d96
Wesley Ramalho <wesleyramalho@Wesleys-MacBook-Pro-2.local>
2019-01-20 16:58:43 -0200

    user info title component ok

---
+++ src/components/UserInfoTitle/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./styles.scss";
+
+const UserInfoTitle = ({ title }) => (
+  <div class="user-info">
+    <h1 class="user-info__title">{title}</h1>
+  </div>
+);
+
+export default UserInfoTitle;
