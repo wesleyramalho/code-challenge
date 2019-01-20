17a15df0ada0ec31b4e549ff4b9b8edf192b2387
Wesley Ramalho <wesleyramalho@Wesleys-MacBook-Pro-2.local>
2019-01-20 21:19:20 -0200

    UserInfoTitle proptypes and bugfix in classname

--- src/components/UserInfoTitle/index.js
+++ src/components/UserInfoTitle/index.js
@@ -1,10 +1,16 @@
 import React from "react";
+import PropTypes from "prop-types";
+
 import "./styles.scss";
 
 const UserInfoTitle = ({ title }) => (
-  <div class="user-info">
-    <h1 class="user-info__title">{title}</h1>
+  <div className="user-info">
+    <h1 className="user-info__title">{title}</h1>
   </div>
 );
 
 export default UserInfoTitle;
+
+UserInfoTitle.propTypes = {
+  title: PropTypes.string.isRequired
+};
