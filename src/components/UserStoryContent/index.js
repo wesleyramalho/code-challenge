e5a3fdc4f17e8571e8ddeda509c77b43879499b5
Wesley Ramalho <wesleyramalho@Wesleys-MacBook-Pro-2.local>
2019-01-21 14:58:03 -0200

    UserStoryContent component structure ok

---
+++ src/components/UserStoryContent/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import "./styles.scss";
+
+const UserStoryContent = ({ stories }) => {
+  const userStories = stories.map(({ name, date, description }) => (
+    <div className="user-story-text-container__content">
+      <h1 className="user-story-text-container__content__name">{name}</h1>
+      <span className="user-story-text-container__content__date">{date}</span>
+      <p className="user-story-text-container__content__description">
+        {description}
+      </p>
+    </div>
+  ));
+  return <div className="user-story-text-container">{userStories}</div>;
+};
+
+export default UserStoryContent;
+
+UserStoryContent.propTypes = {
+  stories: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      date: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired
+    })
+  ).isRequired
+};
