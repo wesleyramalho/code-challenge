718961291a10ba9e5a0e63cf0854d8d23dad3ded
Wesley Ramalho <wesley_raa@hotmail.com>
2019-01-23 14:52:41 -0200

    gsap animation at UserInfoTitle

--- src/components/UserInfoTitle/index.js
+++ src/components/UserInfoTitle/index.js
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
-
+import { TimelineLite } from "gsap/all";
 import "./styles.scss";
 
-const UserInfoTitle = ({ title }) => (
-  <div className="user-info">
-    <h1 className="user-info__title">{title}</h1>
-  </div>
-);
+export default class UserInfoTitle extends Component {
+  constructor(props) {
+    super(props);
 
-export default UserInfoTitle;
+    this.logoTl = new TimelineLite({ paused: true });
+    this.description = null;
+  }
+
+  // add instances to the timeline
+  componentDidMount() {
+    this.logoTl.from(
+      this.description,
+      0.5,
+      { left: 100, autoAlpha: 0 },
+      "feature+=0.25"
+    );
+    this.logoTl.play();
+  }
+
+  render() {
+    const { title } = this.props;
+    return (
+      <div ref={div => (this.description = div)} className="user-info">
+        <h1 className="user-info__title">{title}</h1>
+      </div>
+    );
+  }
+}
 
 UserInfoTitle.propTypes = {
   title: PropTypes.string.isRequired
