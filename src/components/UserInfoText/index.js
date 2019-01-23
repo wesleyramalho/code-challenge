a49a679adebe79083209fc89c6231fe3d635257b
Wesley Ramalho <wesley_raa@hotmail.com>
2019-01-23 14:52:28 -0200

    gsap animation at UserInfoText

--- src/components/UserInfoText/index.js
+++ src/components/UserInfoText/index.js
@@ -1,16 +1,48 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
+import { TimelineLite } from "gsap/all";
+
 import "./styles.scss";
 
-const UserInfoText = ({ text } = []) => {
-  if (!text.length) return null;
-  const textItems = text.map(textItem => {
-    return <h1 key={textItem} className="user-content__text">{textItem}</h1>;
-  });
-  return <div className="user-content">{textItems}</div>;
-};
+export default class UserInfoText extends Component {
+  constructor(props) {
+    super(props);
+    // cards, elements tha will be used in the tween
+    this.cards = [];
+    // the timeline instance
+    this.tl = new TimelineLite({ paused: true });
+  }
+  componentDidMount() {
+    this.tl.staggerTo(this.cards, 0.5, { autoAlpha: 1, y: -20 }, 0.1);
+    this.tl.play(); 
+  }
 
-export default UserInfoText;
+  render() {
+    this.tl
+      .kill()
+      .clear()
+      .pause(0);
+
+    const { text } = this.props;
+    if (!text.length) return null;
+    return (
+      <div className="user-content">
+        {text.map((textItem, index) => {
+          return (
+            <h1
+              key={textItem}
+              className="user-content__text"
+              ref={h1 => (this.cards[index] = h1)}
+            >
+              {textItem}
+            </h1>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
 UserInfoText.propTypes = {
   text: PropTypes.array
 };
