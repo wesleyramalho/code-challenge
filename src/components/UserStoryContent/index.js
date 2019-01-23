449ccb5c9fdacb7f7da6c3cdadc7f19fd65810d4
Wesley Ramalho <wesley_raa@hotmail.com>
2019-01-23 14:52:56 -0200

    gsap animation at UserStoryContent

--- src/components/UserStoryContent/index.js
+++ src/components/UserStoryContent/index.js
@@ -1,22 +1,50 @@
-import React from "react";
 import PropTypes from "prop-types";
-
+import React, { Component } from "react";
+import { TimelineLite } from "gsap/all";
 import "./styles.scss";
 
-const UserStoryContent = ({ stories }) => {
-  const userStories = stories.map(({ name, date, description }) => (
-    <div key={name} className="user-story-text-container__content">
-      <h1 className="user-story-text-container__content__name">{name}</h1>
-      <span className="user-story-text-container__content__date">{date}</span>
-      <p className="user-story-text-container__content__description">
-        {description}
-      </p>
-    </div>
-  ));
-  return <div className="user-story-text-container">{userStories}</div>;
-};
+export default class UserStoryContent extends Component {
+  constructor(props) {
+    super(props);
+    // cards, elements tha will be used in the tween
+    this.cards = [];
+    // the timeline instance
+    this.tl = new TimelineLite({ paused: true });
+  }
+  componentDidMount() {
+    this.tl.staggerTo(this.cards, 0.5, { autoAlpha: 1, y: -20 }, 0.1);
+    this.tl.play()
+  }
 
-export default UserStoryContent;
+  render() {
+    this.tl
+    .kill()
+    .clear()
+    .pause(0);
+
+    const { stories } = this.props;
+    return (
+      
+      <div className="user-story-text-container">
+        {stories.map(({ name, date, description }, index) => (
+          <div
+            key={name}
+            className="user-story-text-container__content"
+            ref={div => (this.cards[index] = div)}
+          >
+            <h1 className="user-story-text-container__content__name">{name}</h1>
+            <span className="user-story-text-container__content__date">
+              {date}
+            </span>
+            <p className="user-story-text-container__content__description">
+              {description}
+            </p>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
 
 UserStoryContent.propTypes = {
   stories: PropTypes.arrayOf(
